3d020316fe2e5f713e603bc38d33e9a72d5e7750
morris <morristwinomugisha2000@gmail.com>
2024-03-20 16:26:50 +0300

    styling the signup section

---
+++ script.js
@@ -0,0 +1,17 @@
+// let password = document.getElementById("password-enter");
+// let passwordConfirm = document.getElementById("password-confirm");
+// const submit = document.querySelector(".create-account-button");
+
+// submit.addEventListener("click", (e) => {validatePassword()});
+
+// function validatePassword() {
+//     if (password.value !== passwordConfirm.value) {
+//         password.setCustomValidity("Invalid field.");
+//         passwordConfirm.setCustomValidity("Invalid field.");
+//         return
+//     } else {
+//     password.setCustomValidity("");
+//     passwordConfirm.setCustomValidity("");
+//     return;
+//     }
+// };
